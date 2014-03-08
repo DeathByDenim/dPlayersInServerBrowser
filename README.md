6e33175be7c87f0712c2a2e04baa61c09326ad68
DeathByDenim <jarno@jarno.ca>
2014-03-08 12:18:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+dPlayersInServerBrowser
+======
+
+Mod for Planetary Annihilation to see who is in a game without actually joining the lobby.
---
+++ ui/mods/dPlayersInServerBrowser/dPlayersInServerBrowser_server_browser.js
@@ -0,0 +1,22 @@
+(function() {
+
+var oldProcessGameBeacon = model.processGameBeacon;
+model.processGameBeacon = function(beacon, host_id) {
+	var game = oldProcessGameBeacon(beacon, host_id);
+	game.player_names = beacon.player_names;
+	
+	return game;
+};
+
+var olddatabind = $('.one-game').attr('data-bind');
+$('.one-game').after(
+	'<!-- ko if: host_id === model.currentSelectedGameHost() -->' +
+	'                    <div class="row one-game" data-bind="' +
+	'                            css: { \'ui-selected\': (host_id == model.currentSelectedGameHost()) },' +
+	'                            click: $parent.setSelected,' +
+	'                            event: { dblclick: $parent.joinSelectedGame }">' +
+	'                        <div class="col-md-12 game"><span style="margin-left:1.2em; margin-right: 1em;">PLAYERS:</span><span data-bind="foreach: $data.player_names"><span data-bind="text: $data" style="margin-right: 1em"></span></span></div>' +
+	'                    </div>' +
+	'<!-- /ko -->'
+);
+})();
